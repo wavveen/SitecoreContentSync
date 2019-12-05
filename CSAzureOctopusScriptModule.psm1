32a8e3e8fe7bab37c491048cfcb4d0088d116194
Arjen van Veen <arjen.van.veen@efocus.nl>
2019-12-05 15:16:28 +0100

    ContentSync

---
+++ CSAzureOctopusScriptModule.psm1
@@ -0,0 +1,29 @@
+function GetKuduConnectionDetailsFromAzurePublishProfile
+{
+	Param(
+		[parameter(Mandatory=$True)]
+		[String]$AzureResourceGroupName,
+		[parameter(Mandatory=$True)]
+		[String]$AzureWebAppName,
+		[parameter(Mandatory=$False)]
+		[String]$AzureWebAppSlot
+	)
+
+	#Get-AzureRmContext is available when this script is running in a Azure Powershell step template context
+	$SubscriptionId = (Get-AzureRmContext).Subscription.SubscriptionId
+
+	if([string]::IsNullOrEmpty($WebAppSlot)){
+		[xml]$PublishProfile = Get-AzureRmWebAppPublishingProfile -ResourceGroupName $AzureResourceGroupName -Name $AzureWebAppName -OutputFile none
+	}else{
+		[xml]$PublishProfile = Get-AzureRmWebAppSlotPublishingProfile -ResourceGroupName $AzureResourceGroupName -Name $AzureWebAppName -Slot $AzureWebAppSlot -OutputFile none
+	}
+	
+	Write-Output "Retrieved PublishProfile: $($PublishProfile.publishData.FirstChild.profileName)"
+	
+	$KuduCredentials = New-Object -TypeName psobject
+	$KuduCredentials | Add-Member -MemberType NoteProperty -Name Username -Value "$($PublishProfile.publishData.FirstChild.userName)"
+	$KuduCredentials | Add-Member -MemberType NoteProperty -Name Password -Value '$($PublishProfile.publishData.FirstChild.userPWD)'
+	$KuduCredentials | Add-Member -MemberType NoteProperty -Name Hostname -Value '$($PublishProfile.publishData.FirstChild.publishUrl)'
+
+	return $KuduCredentials
+}
---
+++ CSResetCheckout.ps1
@@ -0,0 +1,54 @@
+Param(
+	#Root directory of the GIT checkout on the webapp
+	[parameter(Mandatory=$True)]
+	$GitDirectory,
+	
+	#Git content branch name (for example content/Test)
+	[parameter(Mandatory=$True)]
+	$ContentBranch,
+	
+	#Need Kudu connection details
+	[parameter(Mandatory=$False)]
+	[String]$KuduUsername,
+	[parameter(Mandatory=$False)]
+	[String]$KuduPassword,
+	[parameter(Mandatory=$False)]
+	[String]$KuduHostname,
+	
+	#Or Azure environment details
+	[parameter(Mandatory=$False)]
+	[String]$AzureResourceGroupName,
+	[parameter(Mandatory=$False)]
+	[String]$AzureWebAppName,
+	[parameter(Mandatory=$False)]
+	[String]$AzureWebAppSlot
+)
+
+#CSAzureOctopusScriptModule containing functions to get azure details (Only working when this script is running in a Azure Powershell step template context in Octopus)
+#Comment out next line if not running in octopus
+Import-Module -Name "$PSScriptRoot\CSAzureOctopusScriptModule.psm1"
+
+if(![string]::IsNullOrEmpty($AzureResourceGroupName) -And ![string]::IsNullOrEmpty($AzureResourceGroupName)){
+	if([string]::IsNullOrEmpty($AzureWebAppSlot)){
+		$KuduConnectionDetails = GetKuduConnectionDetailsFromAzurePublishProfile -AzureResourceGroupName $AzureResourceGroupName -AzureWebAppName $AzureWebAppName
+	}else{
+		$KuduConnectionDetails = GetKuduConnectionDetailsFromAzurePublishProfile -AzureResourceGroupName $AzureResourceGroupName -AzureWebAppName $AzureWebAppName -AzureWebAppSlot $AzureWebAppSlot
+	}
+	
+	$KuduUsername = $KuduConnectionDetails.Username
+	$KuduPassword = $KuduConnectionDetails.Password
+	$KuduHostname = $KuduConnectionDetails.Hostname
+}
+
+if([string]::IsNullOrEmpty($KuduUsername) -Or [string]::IsNullOrEmpty($KuduPassword) -Or [string]::IsNullOrEmpty($KuduHostname)){
+	Write-Output "Error: Both Kudu connection details and Azure environment details are not provided. Need atleast one of them..."
+	exit 1
+}
+
+#CSKuduScriptFile containing functions to perform Kudu commands on a webapp
+Import-Module -Name "$PSScriptRoot\CSKuduScriptModule.psm1" -Force
+
+RunKuduCommand -Command "git fetch origin" -Directory $GitDirectory -Username $KuduUsername -Password $KuduPassword -Hostname $KuduHostname -RetryAmount 10 -RetryTimespan 120
+RunKuduCommand -Command "git clean -df" -Directory $GitDirectory -Username $KuduUsername -Password $KuduPassword -Hostname $KuduHostname -RetryAmount 10 -RetryTimespan 120
+RunKuduCommand -Command "git reset --hard origin/$ContentBranch" -Directory $GitDirectory -Username $KuduUsername -Password $KuduPassword -Hostname $KuduHostname -RetryAmount 10 -RetryTimespan 120
+RunKuduCommand -Command "git checkout $ContentBranch" -Directory $GitDirectory -Username $KuduUsername -Password $KuduPassword -Hostname $KuduHostname -RetryAmount 10 -RetryTimespan 120
